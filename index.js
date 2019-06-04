9fdcacd0304401f137432fc4feff8af899c91018
Bharath <b.lavendra.95@gmail.com>
2019-06-03 19:30:00 -0500

    Typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const actionRouter = require('/routers/action-router');
 const server = express();
 
 server.use(express.json());
-server.use('./api/projects', projectRouter);
-server.use('./api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.listen(4000);
